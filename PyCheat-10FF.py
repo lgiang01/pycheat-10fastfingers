1c6c72c96930e3b7dd71c7af6bf46a77d5cac7fb
OHaiiBuzzle <giang28062001@outlook.com>
2019-09-15 20:43:33 +0700

    Adding comments and cleaned up the code

--- PyCheat-10FF.py
+++ PyCheat-10FF.py
@@ -4,6 +4,8 @@ from selenium.webdriver.firefox.options import Options
 from selenium import webdriver
 import json, time
 
+# Simulating keyDown and keyUp events for Space. Thanks, StackOverflow!
+
 def dispatchKeyEvent(driver, name, options = {}):
     options["type"] = name
     body = json.dumps({'cmd': 'Input.dispatchKeyEvent', 'params': options})
@@ -11,7 +13,7 @@ def dispatchKeyEvent(driver, name, options = {}):
     url = driver.command_executor._url + resource
     driver.command_executor._request('POST', url, body)
 
-def holdKeyW(driver, duration):
+def holdKeySpace(driver, duration):
     endtime = time.time() + duration
     options = { \
       "code": "Space",
@@ -33,20 +35,30 @@ def holdKeyW(driver, duration):
         options["autoRepeat"] = True
         time.sleep(0.01)
 
+#Starting Chromium and navigate to page
+
 browser = Chrome()
-browser.get('https://10fastfingers.com/typing-test/thai')
-words_list_loc = "words"
+browser.get('https://10fastfingers.com/typing-test/english')
+
+#Locate the input field
 
 type_form_loc = "inputfield"
 type_form = browser.find_element_by_id(type_form_loc) 
 
+# Get value of the "words" array, which, surprisingly, contains all the words needed
+
 lst_words = browser.execute_script("return words;")
 
-time.sleep(15)
+# Sleep for a few seconds. This is to prevent freezes when ads loads
+
+time.sleep(10)
+
+# Run for 65 seconds (Longer than duration of a test to make sure it doesn't stop too soon) then stop
+
 starttime = time.time()
 for _ in range(len(lst_words)):
     type_form.send_keys(lst_words[_])
-    holdKeyW(browser, 0.05)
+    holdKeySpace(browser, 0.05)
     if time.time() - starttime >= 65:
       break
     pass
